31f1372ddbc80fc4adfcc5e7c75a4a8ca9b5f39d
Apple <apple@Apples-MacBook-Pro.local>
2024-02-15 02:31:27 +0530

    new comment

--- examples/data2vec/models/data2vec2.py
+++ examples/data2vec/models/data2vec2.py
@@ -2,7 +2,7 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-
+#check for the commit
 import logging
 import math
 from dataclasses import dataclass, field
